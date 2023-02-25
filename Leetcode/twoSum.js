3a34a755c8964e8d95e54c8ef2bfe7807012e0fe
Thomas Matlock <thomasmatlockbba@gmail.com>
2023-02-25 06:15:46 -0600

    Create twoSum.js

---
+++ Leetcode/twoSum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+    // let result = [];  
+  
+    // for(let i = 0; i < nums.length; i++) {
+    //     for(let j = i + 1; j < nums.length; j++) {
+    //         if(nums[i] + nums[j] === target) {
+    //         result.push(nums[i], nums[j])
+    //         } 
+    //     }
+    // }
+    // console.log(result)
+};
+
+// twoSum([-1, -3, -5, -6], -8);
+twoSum([1, 3, 5, 6], 4);
+twoSum([1, 3, 5, 6], 11);
